e5086692b3d788f9905678b2dbcc220810be8960
giglabo <main@giglabo.com>
2025-04-18 14:52:45 +0200

    added licenses

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   cacheDir: '../../node_modules/.vite/packages/shared',
   plugins: [
     nxViteTsPaths(),
-    nxCopyAssetsPlugin(['*.md']),
+    nxCopyAssetsPlugin(['*.md', '*.txt']),
     dts({ entryRoot: 'src', tsconfigPath: path.join(__dirname, 'tsconfig.lib.json') }),
   ],
   // Uncomment this if you are using workers.

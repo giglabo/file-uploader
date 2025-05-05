68fa02f485989bb99e1c82455c07b896e74d95cb
Elion0512 <alena.yesakova@gmail.com>
2025-05-05 14:03:07 +0200

    Tabs with fragments for links - other variant

---
+++ apps/shared-react-components/src/hooks/useURLFragment.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+export const useURLFragment = (): [string, (newFragment: string) => void] => {
+  const [fragment, setFragment] = useState<string>('');
+
+  useEffect(() => {
+    if (typeof document !== 'undefined') {
+      const currentHash = document.location.hash.replace('#', '');
+      setFragment(currentHash);
+      const handleHashChange = (): void => {
+        const newHash = document.location.hash.replace('#', '');
+        setFragment(newHash);
+      };
+      window.addEventListener('hashchange', handleHashChange);
+      return () => {
+        window.removeEventListener('hashchange', handleHashChange);
+      };
+    }
+    return () => {};
+  }, []);
+
+  const updateFragment = (newFragment: string): void => {
+    if (typeof document !== 'undefined') {
+      if (fragment !== newFragment) {
+        window.history.pushState({}, '', `#${newFragment}`);
+        setFragment(newFragment);
+      }
+    }
+  };
+
+  return [fragment, updateFragment];
+};
